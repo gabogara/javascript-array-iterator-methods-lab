b06cab2f73ab849f8b9d04faf5069a289f07e364
gabogara <gabo1gara@gmail.com>
2025-11-12 21:32:58 -0800

    Add solutions for exercise 7

--- app.js
+++ app.js
@@ -293,5 +293,28 @@ isAdultPresent = devs.some((p) => {
   return 2025 - p.year >= 18;
 });
 // Check your work:
-console.log("Exercise 6 my result: ", isAdultPresent);
+//console.log("Exercise 6 my result: ", isAdultPresent);
 //console.log("Exercise 6 correct result: ", true);
+//-----------------------------------------------------------------------------------------------------------
+/*
+Exercise 7: Array.prototype.every()
+
+Use Array.prototype.every() to determine if every person in the devs array is 
+19 years old or older.
+
+- You have an array of individuals, each with their year of birth represented
+  by the 'year' property.
+- Use the Array.prototype.every() method to verify if every individual in the
+  array is at least 19 years old.
+- Store the result (true or false) in the variable 'isEveryone19OrOlder'.
+*/
+
+let isEveryone19OrOlder = null;
+
+// Complete the exercise in the space below:
+isEveryone19OrOlder = devs.every((d) => {
+  return 2025 - d.year >= 19;
+});
+// Check your work:
+console.log("Exercise 7 my result: ", isEveryone19OrOlder);
+//console.log("Exercise 7 correct result: ", false);
